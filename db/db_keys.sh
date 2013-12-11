a0656a5db63eae882963027d6e7e306aab6975c8
Carlos <wuiscmc@gmail.com>
2013-12-11 01:46:01 +0100

    Support to visualize multiple environments db

--- db/db_keys.sh
+++ db/db_keys.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+env=$1
+if [ "x$env" != "x" ]; then
+  env="$env":
+fi
+namespace="nyhetis:db:"$env
 
-echo "number of relevant news"
-redis-cli KEYS "nyhetis:db:news:all" | xargs redis-cli SCARD
+echo "number of relevant news:" $namespace"news:all" 
+redis-cli KEYS $namespace"news:all" | xargs redis-cli SCARD
 
-echo "bag of words"
-redis-cli KEYS "nyhetis:db:bagwords:words" | xargs redis-cli SMEMBERS
+echo "bag of words: ${namespace}bagwords:words" 
+redis-cli KEYS "${namespace}bagwords:words" | xargs redis-cli SMEMBERS
