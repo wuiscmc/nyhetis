9a2f57831d657507286720dbdf9fb9a8202adf1a
Carlos <wuiscmc@gmail.com>
2013-12-14 23:34:16 +0100

    Updated dossier endpoint spec to cover the DELETE case

--- spec/integration/dossier_endpoint_spec.rb
+++ spec/integration/dossier_endpoint_spec.rb
@@ -4,16 +4,11 @@ require 'typhoeus'
 require 'spec_helper'
 
 describe DossierEndpoint, integration: true do 
-  
-  before do 
-    @config = Psych.load_file("config/dossier.yml")["integration"].freeze
-  end
 
   context "GET /api/v1/news" do
     let(:endpoint) { url("/api/v1/news") } 
     subject{ Typhoeus.get(endpoint) }
     it { should be_success }
-
   end
 
   context "GET /api/v1/search" do
@@ -28,8 +23,20 @@ describe DossierEndpoint, integration: true do
     it { should be_success }
   end
 
+  context "DELETE /api/v1/words" do 
+    let(:endpoint) { url("/api/v1/words") } 
+    subject{ Typhoeus.delete(endpoint) }
+    it { should be_success }
+  end
+
+  context "GET /api/v1/words" do 
+    let(:endpoint) { url("/api/v1/words") } 
+    subject{ Typhoeus.get(endpoint) }
+    it { should be_success }
+  end
+
   def url(endpoint)
-    __url = "#{@config["server"]["host"]}:#{@config["server"]["port"]}"
+    __url = "#{CONFIG["server"]["host"]}:#{CONFIG["server"]["port"]}"
     "#{__url}#{endpoint}"
   end
 
