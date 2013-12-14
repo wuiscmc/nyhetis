597e37c5131d80c58d482ff44a981ced268fc234
Carlos <wuiscmc@gmail.com>
2013-12-14 23:31:19 +0100

    Redis Seeds are no longer needed

--- db/redis_handler.rb
+++ db/redis_handler.rb
@@ -6,8 +6,8 @@ def redis
     $redis ||=  Redis::Namespace.new(CONFIG['database']['namespace'], :redis => Redis.new)
 end
 
-def redis_seeds(env)
-  redis.del(CrawlCommand::REDIS_PREFIX + ":crawling") 
-  words = Psych.load_file("config/words_test.yml")["words"]
-  words.each {|word| redis.sadd(BagOfWords::Word::REDIS_PREFIX, word)}
-end
+ def redis_seeds(env)
+#   redis.del(CrawlCommand::REDIS_PREFIX + ":crawling") 
+#   # words = Psych.load_file("config/words_test.yml")["words"]
+#   # words.each {|word| redis.sadd(BagOfWords::Word::REDIS_PREFIX, word)}
+ end
